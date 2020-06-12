6220bc1a72225109796d8d0f97ca4b3d28d31dd0
Ramya <rboopathiraj@atpco.net>
2020-06-12 18:20:41 -0400

    remove mongo and change server port

--- src/main/java/net/atpco/detour/repository/DetourRepositoryImpl.java
+++ src/main/java/net/atpco/detour/repository/DetourRepositoryImpl.java
@@ -1,18 +1,18 @@
-package net.atpco.detour.repository;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.stereotype.Repository;
-
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-
-@Repository
-@AllArgsConstructor
-@Slf4j
-public class DetourRepositoryImpl implements DetourRepository {
-    @Autowired
-    private MongoTemplate template;
-  
-}
+//package net.atpco.detour.repository;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.data.mongodb.core.MongoTemplate;
+//import org.springframework.stereotype.Repository;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//
+//
+//@Repository
+//@AllArgsConstructor
+//@Slf4j
+//public class DetourRepositoryImpl implements DetourRepository {
+//    @Autowired
+//    private MongoTemplate template;
+//  
+//}
