b520a40a784f5273c1ba170cad6d5ffe0848d2fa
Ramya <rboopathiraj@atpco.net>
2020-06-12 20:17:06 -0400

    added mongoconfig

--- src/main/java/net/atpco/detour/application/MongoConfig.java
+++ src/main/java/net/atpco/detour/application/MongoConfig.java
@@ -1,48 +1,48 @@
 package net.atpco.detour.application;
 
-//import org.springframework.beans.factory.annotation.Value;
-//import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.MongoDbFactory;
-//import org.springframework.data.mongodb.core.MongoTemplate;
-//import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-//import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
-//import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-//import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-//
-//import com.mongodb.MongoClientURI;
-//import com.mongodb.client.MongoClient;
-//import com.mongodb.client.MongoClients;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-//@EnableMongoRepositories
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@EnableMongoRepositories
 @Configuration
 public class MongoConfig {
 	
-//	@Value("${spring.data.mongodb.uri}")
-//	private String mongoUri;
-//
-//	@Bean
-//	public MongoClient mongoClient() {
-//	    return MongoClients.create(mongoUri);
-//	}
-//
-//	@Bean
-//	public MongoDbFactory mongoDbFactory() {
-//		return new SimpleMongoDbFactory(new MongoClientURI(mongoUri));
-//	}
-//
-//	@Bean
-//	public MongoTemplate mongoTemplate() {
-//		return new MongoTemplate(mongoDbFactory());
-//	}
-//
-//	@Bean
-//    public ValidatingMongoEventListener validatingMongoEventListener() {
-//        return new ValidatingMongoEventListener(validator());
-//    }
-//
-//    @Bean
-//    public LocalValidatorFactoryBean validator() {
-//        return new LocalValidatorFactoryBean();
-//    }
+	@Value("${spring.data.mongodb.uri}")
+	private String mongoUri;
+
+	@Bean
+	public MongoClient mongoClient() {
+	    return MongoClients.create(mongoUri);
+	}
+
+	@Bean
+	public MongoDbFactory mongoDbFactory() {
+		return new SimpleMongoDbFactory(new MongoClientURI(mongoUri));
+	}
+
+	@Bean
+	public MongoTemplate mongoTemplate() {
+		return new MongoTemplate(mongoDbFactory());
+	}
+
+	@Bean
+    public ValidatingMongoEventListener validatingMongoEventListener() {
+        return new ValidatingMongoEventListener(validator());
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        return new LocalValidatorFactoryBean();
+    }
 }
