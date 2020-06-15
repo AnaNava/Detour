d6a5d2e65de0c0c5f44dbe9f5aead138a1fe2ac2
cchukka <cchukka@atpco.net>
2020-06-15 18:51:59 -0400

    GET end point working

--- src/main/java/net/atpco/detour/controller/DetourController.java
+++ src/main/java/net/atpco/detour/controller/DetourController.java
@@ -2,9 +2,11 @@ package net.atpco.detour.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -21,12 +23,31 @@ public class DetourController {
 	
 	private final DetourService detourService;
 
-	@RequestMapping("/detour")
+	@RequestMapping(value = "/detour", method = RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<DetourResponse> processRequest(@Valid @RequestBody final DetourRequest detourRequest) {
         log.info("DetourController - processRequest");
         DetourResponse response =  detourService.processRequest(detourRequest);
         return ResponseEntity.ok(response);
     }
+	
+	/*
+	 * "origin":"WAS", "prefDestinaionType":"Beach", "travelFrom":"5Dec",
+	 * "travelTo":"10Dec", "travelPrefType":"Anything",
+	 * "travelType":"International", "distance":"5hrs"
+	 */
+	@RequestMapping(value = "/detour", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<DetourResponse> processRequest() {
+		DetourRequest detourRequest = new DetourRequest();
+		detourRequest.setDistance("5hrs");
+		detourRequest.setOrigin("WAS");
+		detourRequest.setTravelFrom("5Dec");
+		detourRequest.setTravelTo("10Dec");
+		detourRequest.setTravelPrefType("Anything");
+		detourRequest.setPrefDestinaionType("Beach");
+		detourRequest.setTravelType("International");
+		DetourResponse response = detourService.processRequest(detourRequest);
+		return ResponseEntity.ok(response);
+	}
 
    
 
